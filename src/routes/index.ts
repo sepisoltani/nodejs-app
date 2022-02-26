48e224cc4c1c037225b29a4ac65287ec520fbcd2
sepehr <sph_1996@yahoo.com>
2022-02-27 02:07:28 +0330

    add watcher

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.status(200).send('hello world 3');
+    res.status(200).send('hello world 4');
 });
 
 export default router;
